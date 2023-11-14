cfb5fda85ad39a3f139dc395686be3a21fb19270
Savus <justin.fuzzygreenstuff@gmail.com>
2023-11-14 05:36:20 -0700

    first commit. boilerplate html and initializing starting css files.

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "../src/css/styles.css";
+
+function App() {
+  return (
+    <>
+      <main className="transaction-app-wrapper">
+        <div className="transaction-button-container">
+          <button className="transaction-modal-button">
+            Create New Transaction
+          </button>
+        </div>
+        <section className="transaction-history-container">
+          <h3>Transaction History</h3>
+          <ul className="record-list"></ul>
+        </section>
+      </main>
+
+      <section className="modal-section">
+        <div className="full-site-modal">
+          <header className="modal-header">
+            <div className="current-balance">
+              <h3>Current Balance: 0.00</h3>
+            </div>
+            <div className="close-modal-container">
+              <i className="fa-solid fa-x"></i>
+            </div>
+          </header>
+          <div className="modal-body">
+            <form action="" className="new-transaction-form">
+              <div className="form-divider left-side">
+                <div className="input-container">
+                  <label htmlFor="new-balance">Enter New Balance: --</label>
+                  <input type="text" placeholder="0.00" />
+                </div>
+              </div>
+              <div className="form-divider right-side">
+                <h3 className="balance-to-submit">New Balance: 0.00</h3>
+                <button id="record-transaction">Record Transaction</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+      
+    </>
+  );
+}
+
+export default App;
