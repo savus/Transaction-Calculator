8411b2c24d097822d175d0c349013eab5b819579
Savus <justin.fuzzygreenstuff@gmail.com>
2023-11-15 07:15:53 -0700

    style new-transaction modal, add transitions, and is-visible toggle state for create new transaction button.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "../src/css/theme.css";
 import "../src/css/styles.css";
 
 function App() {
+  const [transactionModalVisible, setTransactionModalVisible] = useState("");
   return (
     <>
       <main className="transaction-app-wrapper container-md">
@@ -11,6 +12,8 @@ function App() {
           <button
             id="transaction-modal-button"
             className="btn spread-out-transition"
+            data-open="new-transaction"
+            onClick={() => setTransactionModalVisible("is-visible")}
           >
             Create New Transaction
           </button>
@@ -21,27 +24,49 @@ function App() {
         </section>
       </main>
 
-      <section className="modal-section">
-        <div className="full-site-modal is-visible">
+      <section
+        className={`modal-section ${transactionModalVisible}`}
+        id="new-transaction"
+      >
+        <div className="modal-content container-md">
           <header className="modal-header">
             <div className="current-balance">
               <h3>Current Balance: 0.00</h3>
             </div>
             <div className="close-modal-container">
-              <i className="fa-solid fa-x"></i>
+              <i
+                className="fa-solid fa-x"
+                data-close="new-transaction"
+                onClick={() => setTransactionModalVisible("")}
+              ></i>
             </div>
           </header>
           <div className="modal-body">
             <form action="" className="new-transaction-form">
               <div className="form-divider left-side">
                 <div className="input-container">
-                  <label htmlFor="new-balance">Enter New Balance: --</label>
+                  <label htmlFor="new-balance">
+                    Change Current Balance: --
+                  </label>
+                  <input type="text" placeholder="0.00" />
+                </div>
+                <div className="input-container">
+                  <label htmlFor="new-balance">Amount to Add: --</label>
+                  <input type="text" placeholder="0.00" />
+                </div>
+                <div className="input-container">
+                  <label htmlFor="new-balance">Amount to Subtract: --</label>
                   <input type="text" placeholder="0.00" />
                 </div>
               </div>
               <div className="form-divider right-side">
                 <h3 className="balance-to-submit">New Balance: 0.00</h3>
-                <button id="record-transaction">Record Transaction</button>
+                <button
+                  id="record-transaction"
+                  className="btn spread-out-transition"
+                >
+                  Record Transaction
+                </button>
               </div>
             </form>
           </div>
