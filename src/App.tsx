82e40cea2d4292cc56f03b6d6f9bce21b578ba81
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2023-12-09 13:24:00 -0700

    start over

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,8 @@
 import { Component } from "react";
-import "./css/base.css";
-import "./css/styles.css";
-import { ModalButton } from "./components/ModalButton";
-import { ButtonContainer } from "./components/shared/ButtonContainer";
-import { ModalComponent } from "./components/ModalComponent";
-import { Requests } from "./api";
-import { Transaction } from "./types";
 
-type State = {
-  modalVisibleState: string;
-  allTransactions: Transaction[];
-};
-
-const isVisible = "is-visible";
-
-class App extends Component<Record<string, never>, State> {
-  state: State = {
-    modalVisibleState: isVisible,
-    allTransactions: [],
-  };
-
-  refetchData = () => {
-    return Requests.getAllTransactions().then((responses) => {
-      this.setState({ allTransactions: responses });
-    });
-  };
-
-  componentDidMount(): void {}
+class App extends Component<Record<string, never>> {
   render() {
-    const { modalVisibleState } = this.state;
-    return (
-      <>
-        <main className="container-md transaction-container">
-          <header className="transaction-header">
-            <ButtonContainer>
-              <ModalButton
-                text={"Create New Transaction"}
-                onClick={() => {
-                  this.setState({ modalVisibleState: isVisible });
-                }}
-              />
-            </ButtonContainer>
-          </header>
-        </main>
-        <ModalComponent
-          modalVisibleState={modalVisibleState}
-          setModalVisibleState={(modalVisibleState) => {
-            this.setState({ modalVisibleState: modalVisibleState });
-          }}
-          isVisible={isVisible}
-        />
-      </>
-    );
+    return <></>;
   }
 }
 
